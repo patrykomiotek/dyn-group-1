25a21cfc5b006659325c6456b7de2d22060babe0
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-09-30 13:41:58 +0200

    feat: with state as an object

--- src/components/FormState.tsx
+++ src/components/FormState.tsx
@@ -1,17 +1,37 @@
-import { useState, type FormEventHandler } from "react";
+import {
+  useState,
+  type ChangeEventHandler,
+  type FormEventHandler,
+} from "react";
 import { Button } from "../ui/Button";
 
 export function FormState() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [favLanguage, setFavLanguage] = useState("");
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [favLanguage, setFavLanguage] = useState("");
+  const [formState, setFormState] = useState({
+    email: "",
+    password: "",
+    favLanguage: "",
+  });
+
+  const { email, password, favLanguage } = formState;
 
   const handleSubmit: FormEventHandler = (event) => {
     event.preventDefault();
     console.log({ email, password, favLanguage });
   };
 
-  // handleChange
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const value = event.target.value;
+    // const name = event.target.name;
+    const id = event.target.id;
+
+    setFormState({
+      ...formState,
+      [id]: value,
+    });
+  };
 
   return (
     <form onSubmit={handleSubmit}>
@@ -22,27 +42,15 @@ export function FormState() {
       </div>
       <div>
         <label htmlFor="email">E-mail</label>
-        <input
-          id="email"
-          type="email"
-          onChange={(event) => setEmail(event.target.value)}
-        />
+        <input id="email" type="email" onChange={handleChange} />
       </div>
       <div>
         <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          onChange={(event) => setPassword(event.target.value)}
-        />
+        <input id="password" type="password" onChange={handleChange} />
       </div>
       <div>
         <label htmlFor="favLanguage">Favorite language</label>
-        <input
-          id="favLanguage"
-          type="text"
-          onChange={(event) => setFavLanguage(event.target.value)}
-        />
+        <input id="favLanguage" type="text" onChange={handleChange} />
       </div>
       <Button type="submit">Submit</Button>
     </form>
