8207c112bbe45b13b40c748da8e9117099100d5b
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-09-30 10:00:10 +0200

    feat: with types

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
+// import { Fragment } from "react";
 import "./App.css";
 
 import { Text } from "./ui/Text";
 
 function App() {
   return (
-    <div>
-      <Text>Hello World!</Text>
-    </div>
+    <>
+      <div>
+        <Text>Hello World!</Text>
+      </div>
+      <div>
+        <Text>Hello World!</Text>
+      </div>
+    </>
   );
 }
 
