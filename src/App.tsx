3f41f5c9e86a7ec3183e784ac586c8e09079eded
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-01 09:49:56 +0200

    feat: with react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,11 @@
-// import { Fragment } from "react";
-import "./App.css";
-import { FormRefsRHF } from "./components/FormRefsRHF";
-import { RegistrationPage } from "./pages/RegistrationPage";
-// import { FormRefs } from "./components/FormRefs";
-// import { FormState } from "./components/FormState";
-// import { Generator } from "./components/Generator";
-// import { Button } from "./ui/Button";
+import { RouterProvider } from "react-router-dom";
 
-// import { Text } from "./ui/Text";
+import { router } from "./routes";
 
 function App() {
   return (
     <div className="p-4">
-      {/* <div>
-        <Text>Hello World!</Text>
-      </div>
-      <Button color="clouds" bgColor="carrot">
-        Click me
-      </Button>
-      <Button>Click me too</Button> */}
-      {/* <Generator /> */}
-      {/* <FormState /> */}
-      {/* <FormRefs /> */}
-      {/* <FormRefsRHF /> */}
-      <RegistrationPage />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/routes.tsx
@@ -0,0 +1,36 @@
+// browserRouter - /registration, /sign-up
+// hashRouter - /#registration
+// memoryRouter - /
+import { createBrowserRouter } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { RegistrationPage } from "./pages/RegistrationPage";
+
+type Route = Record<
+  string,
+  {
+    path: string;
+    title: string;
+  }
+>;
+
+export const Route: Route = {
+  HOME: {
+    path: "/",
+    title: "Home",
+  },
+  REGISTRATION: {
+    path: "/registration",
+    title: "Registration",
+  },
+};
+
+export const router = createBrowserRouter([
+  {
+    path: Route.HOME.path, // "/"",
+    element: <HomePage />,
+  },
+  {
+    path: Route.REGISTRATION.path,
+    element: <RegistrationPage />,
+  },
+]);
