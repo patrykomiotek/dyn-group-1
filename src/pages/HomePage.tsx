acf25e3e2ccd6edc06e669495d88069e74397dcf
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-01 11:40:48 +0200

    feat: with calue collector component

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
 import { Counter } from "../components/Counter";
+import { ValueCollector } from "../components/ValueCollector";
 import { ViewPort } from "../components/ViewPort";
 
 export function HomePage() {
   return (
     <div className="space-y-4">
       <h1 className="text-3xl">Home Page</h1>
+      <ValueCollector />
       <ViewPort />
       <Counter />
     </div>
