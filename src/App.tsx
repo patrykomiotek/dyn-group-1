b4174155c9b5e307f7a7da2cdccfb09fc9012cce
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-02 12:20:01 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60 * 5, // 5 hours
+      staleTime: 1000 * 60 * 5, // 5 minutes
       retry: 1,
     },
   },
