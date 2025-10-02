656b6a9e9ebe61d1af7e72f16910cfa2f5fcdd4a
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-02 09:50:54 +0200

    feat: with product fetch
    feat: with token
    
    feat: hide token

--- src/pages/ProductsListPage.tsx
+++ src/pages/ProductsListPage.tsx
@@ -1,7 +1,63 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface ApiListResponse<T> {
+  records: T[];
+}
+
+interface ProductDto {
+  id: number;
+  fields: {
+    name: string;
+    description: string;
+    price: number;
+  };
+}
+
+// webpack: process.env.API_TOKEN
+// vite: import.meta.env.VITE_PUBLIC_API_TOKEN
+const API_TOKEN = import.meta.env.VITE_API_TOKEN || "";
+
 export function ProductsListPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [data, setData] = useState<ProductDto[]>([]);
+
+  const loadData = async () => {
+    console.log({ API_TOKEN, otherVars: import.meta.env });
+    try {
+      const response = await axios.get<ApiListResponse<ProductDto>>(
+        "https://api.airtable.com/v0/appn8kDF6gXKpZ00E/products",
+        {
+          headers: {
+            Authorization: `Bearer ${API_TOKEN}`,
+          },
+        }
+      );
+      setData(response.data.records);
+    } catch {
+      setIsError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
   return (
     <div className="space-y-4">
       <h1 className="text-3xl">Products List</h1>
+
+      {isLoading && <p>Loading...</p>}
+      {isError && <p>Component error</p>}
+      {data.map((elem) => (
+        <div key={elem.id} className="space-y-6">
+          <h2 className="text-2xl">{elem.fields.name}</h2>
+          <p>{elem.fields.description}</p>
+        </div>
+      ))}
     </div>
   );
 }
