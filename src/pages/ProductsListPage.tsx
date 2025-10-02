b815ffeb95f59dcb77ab0c249e049c20ccdef327
Patryk Omiotek <patryk.omiotek@gmail.com>
2025-10-02 10:56:34 +0200

    feat: with useApi hook

--- src/pages/ProductsListPage.tsx
+++ src/pages/ProductsListPage.tsx
@@ -1,26 +1,30 @@
+import { useApi } from "@/hooks/useApi";
 import { fetchProducts } from "@/services/products";
 import type { ProductDto } from "@/types/api";
-import { useEffect, useState } from "react";
 
 export function ProductsListPage() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const [data, setData] = useState<ProductDto[]>([]);
+  const { data, isLoading, isError } = useApi<ProductDto[]>(fetchProducts);
 
-  const loadData = async () => {
-    try {
-      const response = await fetchProducts();
-      setData(response.records);
-    } catch {
-      setIsError(true);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+  // const records = data?.records
 
-  useEffect(() => {
-    loadData();
-  }, []);
+  // const [isLoading, setIsLoading] = useState(true);
+  // const [isError, setIsError] = useState(false);
+  // const [data, setData] = useState<ProductDto[]>([]);
+
+  // const loadData = async () => {
+  //   try {
+  //     const response = await fetchProducts();
+  //     setData(response.records);
+  //   } catch {
+  //     setIsError(true);
+  //   } finally {
+  //     setIsLoading(false);
+  //   }
+  // };
+
+  // useEffect(() => {
+  //   loadData();
+  // }, []);
 
   return (
     <div className="space-y-4">
@@ -28,12 +32,13 @@ export function ProductsListPage() {
 
       {isLoading && <p>Loading...</p>}
       {isError && <p>Component error</p>}
-      {data.map((elem) => (
-        <div key={elem.id} className="space-y-6">
-          <h2 className="text-2xl">{elem.fields.name}</h2>
-          <p>{elem.fields.description}</p>
-        </div>
-      ))}
+      {data &&
+        data.map((elem) => (
+          <div key={elem.id} className="space-y-6">
+            <h2 className="text-2xl">{elem.fields.name}</h2>
+            <p>{elem.fields.description}</p>
+          </div>
+        ))}
     </div>
   );
 }
